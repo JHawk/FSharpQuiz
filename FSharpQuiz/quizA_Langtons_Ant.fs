f770579e14525ca9979060d14fa0079c8d5d4ca6
Josh Hawkins <joshua.hawkins@benchmarksolutions.com>
2012-07-18 16:34:28 -0400

    added step count

--- FSharpQuiz/quizA_Langtons_Ant.fs
+++ FSharpQuiz/quizA_Langtons_Ant.fs
@@ -1,6 +1,7 @@
 ﻿namespace FSharpQuiz.LangtonsAnt
 
     open System
+    open FSharpQuiz.General
     
     [<AutoOpen>]
     module internal LangtonsAntUtils =
@@ -22,31 +23,33 @@
             | p when 0 > p.x || p.x > grid_size || 0 > p.y || p.y > grid_size -> false
             | _ -> true
 
-        let render grid_size black_positions' pos' = 
+        let render grid_size black_positions pos count = 
             [ for x in 0..grid_size do 
                 for y in 0..grid_size do
                     let p = {x = x ; y = y}
-                    yield if p = pos' then "A"
-                          elif black_positions' |> Set.contains p then "#" 
+                    yield if p = pos then "A"
+                          elif black_positions |> Set.contains p then "#" 
                           else "."
-                yield "\n" ] |> List.reduce (+) |> System.Console.WriteLine
+                yield (count.ToString()) + " steps\n" ] |> List.reduce (+) |> System.Console.WriteLine
 
-        let move grid_size black_positions pos (dir: Direction) =
+        let move grid_size watch =
             let on_grid' = on_grid grid_size
             let render' = render grid_size
+            let black_positions = Set.empty 
+            let pos = {x = grid_size/2 ; y = grid_size/2}
 
-            let rec move' black_positions pos (dir: Direction) =
+            let rec move' black_positions pos (dir: Direction) count =
                 let black_positions', dir' =
                     if black_positions |> Set.contains pos then
                         Set.remove pos black_positions, dir.left
                     else 
                         Set.add pos black_positions, dir.right
                 let pos' = dir'.move pos
-                if not <| on_grid' pos' then render' black_positions' pos'
-                else move' black_positions' pos' dir'
-            move' black_positions pos dir
-
-        let start grid_size = move grid_size Set.empty {x = grid_size/2 ; y = grid_size/2} Direction.Rand
+                if not <| on_grid' pos' then render' black_positions' pos' count
+                else 
+                    if watch then render' black_positions' pos' count
+                    move' black_positions' pos' dir' (count + 1L)
+            move' black_positions pos Direction.Rand 0L
 
     type LangtonsAnt () = 
         interface FSharpQuiz.General.IQuiz with
@@ -54,9 +57,10 @@
             member __.description = "A) Langton's Ant"
             member __.start () = 
                 Console.WriteLine("Enter the length of the sides of the grid.")
+                Console.WriteLine("For best results 100.")
                 let rec loop () = 
                     match Int32.TryParse <| Console.ReadLine() with
-                        | true, i -> start i
+                        | true, i -> move i <| _continue ("Watch the ant?")
                         | _       -> Console.WriteLine("Come on! An int plz.")
                                      loop ()
                 loop ()
